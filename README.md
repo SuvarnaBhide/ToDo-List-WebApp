dfcbb6a0cf81cece7468ba71802bdedae2675dd8
Suzie <its.suzie.here@gmail.com>
2023-08-28 18:14:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Todolist
+
+Todolist is a simple web-based to-do list application that allows you to manage your tasks. You can add new items, mark completed tasks, and delete items from the list.
+
+## Installation
+
+1. Clone or download the repository.
+2. Open the `index.html` file in your web browser.
+
+## How to Use
+
+1. View the default to-do list with some sample tasks.
+2. To add a new item, type the task in the input field and press the "+" button.
+3. To mark a task as completed, check the checkbox next to the task. The task text will be crossed out.
+4. To delete a task, click the "<--" button next to the task.
+5. You can also create custom to-do lists by appending the list name to the URL (e.g., http://localhost:3000/custom-list-name).
+
+## Live Demo
+
+Try out the Todolist-v1 [here](https://fine-lab-coat-fish.cyclic.app/).
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Express.js
+- MongoDB
+- EJS (Embedded JavaScript)
+
+## Contributing
+
+Contributions to this project are welcome! If you have any suggestions, bug fixes, or improvements, feel free to create a pull request.
+
+## License
+
+This project is licensed under the [ISC License](https://opensource.org/licenses/ISC).
+
+## Contact
+
+For questions or feedback, please feel free to contact me at jatinsingh0701@gmail.com. I'm looking forward to hearing from you and making this project even better!
---
+++ app.js
@@ -0,0 +1,130 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const _ = require("lodash");
+const date = require(path.join(__dirname, "/date.js"));
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware and Configuration
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+mongoose.set('strictQuery', false);
+
+// Connect to MongoDB database
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://suvarna_bhide:suvarna_bhide@cluster0.5rjgpx9.mongodb.net/todolistDB");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+// Define Mongoose schemas and models
+const itemsSchema = new mongoose.Schema({ name: String });
+const Item = mongoose.model("Item", itemsSchema);
+const defaultItems = [ {
+  name: "Welcome to your todolist!",
+},
+{
+  name: "Hit the + button to add a new item.",
+},
+{
+  name: "<-- Hit this to delete an item.",
+},];
+const listSchema = new mongoose.Schema({ name: String, items: [itemsSchema] });
+const List = mongoose.model("List", listSchema);
+
+// Home Route
+app.get("/", async function (req, res) {
+  try {
+    const foundItems = await Item.find({});
+    if (foundItems.length === 0) {
+      await Item.insertMany(defaultItems);
+      console.log("Successfully saved default items to DB");
+      res.redirect("/");
+    } else {
+      const day = date.getDate();
+      res.render("list", { listTitle: day, newListItems: foundItems });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Custom List Route
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+  List.findOne({ name: customListName })
+    .then((foundList) => {
+      if (!foundList) {
+        const list = new List({ name: customListName, items: defaultItems });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+// Create New Item
+app.post("/", function (req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({ name: itemName });
+  if (listName === date.getDate()) {
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }).then((foundList) => {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+// Delete Item
+app.post("/delete", async (req, res) => {
+  try {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+    if (listName === date.getDate()) {
+      await Item.findByIdAndDelete(checkedItemId);
+      console.log("Successfully deleted checked item.");
+      res.redirect("/");
+    } else {
+      const foundList = await List.findOne({ name: listName });
+      foundList.items.pull({ _id: checkedItemId });
+      await foundList.save();
+      res.redirect("/" + listName);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error")
+  }
+});
+
+// About Route
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+// Connect to MongoDB and start the server
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+});
+
